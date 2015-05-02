f485fa0366f852f74c3b6cdf6f018da925113b14
henrykh <henryhowes@gmail.com>
2015-05-02 13:52:39 -0700

    bug fixes, adds repr for Card

--- game.py
+++ game.py
@@ -3,19 +3,20 @@ import random
 
 class Player():
     def __init__(self):
-        self.hand = Hand()
+        self.hand = []
 
 
 class Game():
 
     def __init__(self):
+        self.won = False
         self.deck = Deck()
         self.deck.shuffle()
 
         # Init players, pick start player
         self.player1 = Player()
         self.player2 = Player()
-        self.currentPlayer = random.choice[self.player1, self.player2]
+        self.currentPlayer = random.choice([self.player1, self.player2])
 
         # Deal hands
         for i in range(7):
@@ -23,17 +24,23 @@ class Game():
             self.deck.draw(self.player2)
 
         # initialize flags, flag control tracking
-        self.flags = [[] for i in range(7)]
+        self.flags = [[[], []] for i in range(7)]
         self.flag_control = []*7
+
+    # print out the board state
+    def display(self):
+        for flag in self.flags:
+            for stack in flag:
+                print " ".join(stack) + " | "
 
 
 class Deck():
-    
+
     def __init__(self):
         self._deck = [Card(val, color) for val in range(1, 11)
-                         for color in ["red", "green",
-                                       "blue", "purple",
-                                       "yellow", "orange"] ]
+                      for color in ["R", "G",
+                                    "B", "P",
+                                    "Y", "O"]]
 
     def draw(self, player):
         card = self._deck.pop()
@@ -49,15 +56,10 @@ class Card():
         self.color = color
 
     def __str__(self):
-        return "{} {}".format(self.val, self.color)
+        return "{}{}".format(self.val, self.color)
 
-
-class Hand():
-    def __init__(self):
-        self._hand = []
-
-
+    def __repr__(self):
+        return self.__str__()
 
 if __name__ == "__main__":
     game = Game()
-
