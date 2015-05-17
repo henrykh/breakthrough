aefafba88260a85ca6801466e82b2e35659a2c3c
henrykh <henryhowes@gmail.com>
2015-05-17 16:33:15 -0700

    adds test_game, pytest, requirements.txt

---
+++ test_game.py
@@ -0,0 +1,14 @@
+from game import Game, Deck, Card, Player
+import pytest
+
+
+def test_card_constructor():
+    test_card = Card(10, 'G')
+    assert test_card.val == 10
+    assert test_card.color == 'G'
+
+
+def test_deck_constructor():
+    test_deck = Deck()
+    assert len(test_deck._deck) == 60
+
